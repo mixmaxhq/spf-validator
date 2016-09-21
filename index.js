5efbf7b72868598c998bf6924a3a31f883941652
Trey Tacon <ttacon@gmail.com>
2016-09-21 14:57:39 -0400

    Add jsdocs

--- index.js
+++ index.js
@@ -4,11 +4,20 @@ const dns = require('dns');
 const _ = require('underscore');
 const sync = require('synchronize');
 
+/**
+ * SPFValidator is a validator of SPF records for the given domain.
+ * @param {String} domain The domain of interest.
+ */
 function SPFValidator(domain) {
   this._domain = domain;
 }
 
 Object.assign(SPFValidator.prototype, {
+  /**
+   * hasRecords checks if the domain has any SPF records setup at all.
+   * @param  {Function} done Node style callback.
+   * @return {Boolean} True if the domain has *any* SPF records, false otherwise.
+   */
   hasRecords: function(done) {
     sync.fiber(() => {
       var vals = sync.await(dns.resolveTxt(this._domain, sync.defer()));
